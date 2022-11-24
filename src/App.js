0e069fc21a06770a0a177ee007678265f1121b13
Labeeb Latheef <43567561+labeebklatif@users.noreply.github.com>
2022-11-24 21:29:18 +0530

    cleanup

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./Home";
 import Away from "./Away";
-import { useEffect } from "react";
+
 
 function App() {
 
